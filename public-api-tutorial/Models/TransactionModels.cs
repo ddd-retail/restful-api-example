f56a759758d03f860ca3b064e680d3241e479d62
Martin Sax <martin.sax@live.com>
2016-08-31 10:40:22 +0200

    initial commit

---
+++ public-api-tutorial/Models/TransactionModels.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace public_api_tutorial.Models
+{
+    public class TransactionContainerViewModel
+    {
+        public TransactionSalesViewModel Sales { get; set; }
+        public IEnumerable<CorrectionTransactionViewModel> Corrections { get; set; }
+        public IEnumerable<PurchaseTransactionViewModel> Purchases { get; set; }
+        public IEnumerable<TransferTransactionViewModel> Transfers { get; set; }
+    }
+
+    public class TransactionSalesViewModel
+    {
+        public IEnumerable<PosItemTransaction> SaleItems { get; set; }
+
+        public IEnumerable<PosPaymentTransaction> SalePayment { get; set; }
+    }
+    public class PosItemTransaction
+    {
+        public decimal CostPrice { get; set; }
+    }
+    public class PosPaymentTransaction
+    {
+
+    }
+    public class CorrectionTransactionViewModel
+    {
+
+    }
+    public class PurchaseTransactionViewModel
+    {
+        public int SupplierOrderNumber { get; set; }
+
+        public int MachineNumber { get; set; }
+        public string InvoiceNumber { get; set; }
+        public int UserId { get; set; }
+        public decimal SuggestedSalesPrice { get; set; }
+        public decimal Discount { get; set; }
+        public string CurrencyCode { get; set; }
+        public decimal ExchangeRateToLocalCurrency { get; set; }
+        public decimal ExchangeRateToEUR { get; set; }
+        public decimal CostPrice { get; set; }
+        public int UniqueItemNumber { get; set; }
+        public int SupplierId { get; set; }
+        public int EAN { get; set; }
+        public decimal WholeSaleCostPrice { get; set; }
+        public int ShopId { get; set; }
+        public int TransactionNumber { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+        public int TransactionType { get; set; }
+        public int Quantity { get; set; }
+        public int ItemGroupNumber { get; set; }
+    }
+
+    public class TransferTransactionViewModel
+    {
+
+    }
+
+    public enum CorrectionTypeViewModel
+    {
+        Unknown = 0,
+        Manual = 1,
+        Status = 2
+    }
+}
---
+++ public-api-tutorial/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using public_api_tutorial.Models;
+
+namespace public_api_tutorial
+{
+    class Program
+    {
+
+        //see documentation http://publicapi.dddretail.com/
+
+
+        private const string baseUrl = "http://publicapi.dddretail.com/api/";
+        private const string service = "transactions/";
+        public const int shopId = 996005;
+        private const string token = "your token";
+        
+        static void Main(string[] args) 
+        {
+            GetTransactions();
+        }
+
+        private static void GetTransactions()
+        {
+            using (var client = new WebClient())
+            {
+                client.Headers = new WebHeaderCollection
+                {
+                    { HttpRequestHeader.ContentType,"application/json" },
+                    { HttpRequestHeader.Authorization, $"ddd_token {token}"}
+                };
+                var timeOfTransactions = new DateTime(2015,1,1);
+                var requestUrl = baseUrl + service + shopId + $"/{timeOfTransactions.ToString("yyyy-MM-dd")}"; // see documentation http://publicapi.dddretail.com/Help/Api/GET-api-transactions-shopId-from
+                var jsonResult = client.DownloadString(requestUrl);
+
+                //using dynamics for fast deserializion
+                var myDynamicResult = JsonConvert.DeserializeObject<dynamic>(jsonResult);
+                var dynamicSales = myDynamicResult.sales.saleItems;
+                foreach (var sale in dynamicSales)
+                {
+                    //notice the property name costPrice, this is due to the deserializion of the dynamic type
+                    var price = sale.costPrice;
+                }
+
+                //using typed deserializion IMPORTENT:: The viewmodel is not fully implemented, only needed properties for this project is implemented.
+                var myTypedResult = JsonConvert.DeserializeObject<TransactionContainerViewModel>(jsonResult);
+                var typedSales = myTypedResult.Sales.SaleItems;
+                foreach (var sale in typedSales)
+                {
+
+                    var price = sale.CostPrice;
+                }
+            }
+        }
+        
+
+    }
+
+}
